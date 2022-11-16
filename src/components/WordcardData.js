edf9880f5197c2eebf95e7c689dabe79706f8cfb
ArunJp0 <prasathjp95@gmail.com>
2022-11-16 18:29:43 +0530

    newchange2

--- src/components/WordcardData.js
+++ src/components/WordcardData.js
@@ -2,6 +2,7 @@ import pro1 from '../assets/project1.png';
 import pro3 from '../assets/project3.png';
 import project_2 from '../assets/project_2.PNG'
 import project5 from '../assets/project5.PNG'
+import project6 from '../assets/project6.PNG'
 
 const ProjectCardData = [
     {
@@ -29,10 +30,10 @@ const ProjectCardData = [
         view: "https://arunjptodolist.herokuapp.com/"
     },
     {
-        imgsrc: project_2,
-        title: "Adyar Academy Website",
-        text: "This website is built by HTML, CSS, and JAVASCRIPT, and PHP. Academy is providing coaching for competitive examination for UPSC / TNPSC and Customized Competency and skill development bridge training for Industrial requirement.",
-        view: "https://adyaracademy.com"
+        imgsrc: project6,
+        title: "Sample HTML Website",
+        text: "This website is built by HTML, CSS, and JAVASCRIPT. This is a sample practice HTML website.",
+        view: "https://arunjp0.github.io/edufold/index.html"
     },
     {
         imgsrc: pro3,
